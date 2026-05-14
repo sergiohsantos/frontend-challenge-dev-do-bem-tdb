a8615885c2c5be534ad255a83fa4186bff4c4880
sergiohsantos <rm567254@fiap.com.br>
2026-05-14 16:05:55 -0300

    Add ScrollToTop

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { Providers } from './components/providers'
 import { SkipLink } from './components/accessibility/skip-link'
+import { ScrollToTop } from './components/navigation/scroll-to-top'
 import { WatsonAssistant } from './components/watson-assistant'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+      <ScrollToTop />
       <Providers>
         <SkipLink />
         <div id="main-content">
