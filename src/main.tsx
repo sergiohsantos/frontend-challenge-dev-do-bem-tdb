7bba0c13d01a4f075e43a6d00bb360ec2abcb447
sergiohsantos <rm567254@fiap.com.br>
2026-05-14 10:31:27 -0300

    Fix Vlibras

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import App from './App'
 import { Providers } from './components/providers'
 import { SkipLink } from './components/accessibility/skip-link'
 import { WatsonAssistant } from './components/watson-assistant'
-import { VLibrasWidget } from './components/accessibility/vlibras-widget'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -17,7 +16,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           <App />
         </div>
         <WatsonAssistant />
-        <VLibrasWidget />
       </Providers>
     </BrowserRouter>
   </React.StrictMode>,
